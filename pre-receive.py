dddb728e09c6ac7b2d57939dde356c14c9225a3f
Guillaume ANTON <49228243+Guillaume-ANTON@users.noreply.github.com>
2019-05-25 14:53:29 +0200

    Create pre-receive.py

---
+++ pre-receive.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python2
+import sys
+import re
+import subprocess
+import os
+
+#Format: "oldref newref branch"
+
+verifline=sys.stdin.read()
+files = repos.diff(oldref, newref)
+
+for i in files():
+        temp = open('Temp.tmp', 'w')
+        temp.close
+
+def iteration(i):
+        while i > 0:
+                print(i)
+                i = i-1
+        print('Itération ok')
+
+# Boucle en Francais car j'arrive pas en python
+
+#if mon_fichier est un fichier python:
+#       mon-fichier ce stocke dans le dossier /temp
+#elseif mon_fichier n'est pas un fichier python:
+        passer à un autre fichier
+
+
+#/usr/bin/python2 -m py_compile
+
